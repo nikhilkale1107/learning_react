ff715642988e6a21be94fd090f7dbe5c79ffd01b
Nikhil Kale <nikhilkale1107@gmail.com>
2024-10-06 11:02:44 +0530

    Styling Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,12 @@
+import './ExpenseItem.css';
 function ExpenseItem(){
   return(
-    <div>
-    <div>Date</div>
-    <div>"Delhi"</div>
+    <div className='expense-item'>
+    <div>11 July 2024</div>
+    <div className='expense-item__description'>
+        <div>Book</div>
+        <div className='expense-item__price'>$10</div>
+    </div>
     </div>
   )
 }
