677f3e295a6575232f3d3988ddf811b0a099573a
Nikhil Kale <nikhilkale1107@gmail.com>
2024-10-06 11:49:12 +0530

    Props Object & Passing Data to Child Component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div>
       <h1>Let's get Started</h1>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem date={new Date(2024, 7, 11)} title='Insurance' price='105'></ExpenseItem>
+      <ExpenseItem date={new Date(2024, 6, 11)} title='Book' price='100'></ExpenseItem>
+      <ExpenseItem date={new Date(2024, 5, 11)} title='Pen' price='10'></ExpenseItem>
+      <ExpenseItem date={new Date(2024, 4, 11)} title='Laptop' price='1000'></ExpenseItem>
     </div>
   )
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,11 @@
 import './ExpenseItem.css';
-function ExpenseItem(){
-  const expenseDate = new Date(2024, 7, 11).toISOString();
-  const expenseTitle = "Book";
-  const expenseAmount = 10;
+function ExpenseItem(props){
   return(
     <div className='expense-item'>
-    <div>{expenseDate}</div>
+    <div>{props.date.toISOString()}</div>
     <div className='expense-item__description'>
-        <div>{expenseTitle}</div>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <div>{props.title}</div>
+        <div className='expense-item__price'>${props.price}</div>
     </div>
     </div>
   )
